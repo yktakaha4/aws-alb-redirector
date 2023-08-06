44d2b58b0dbe9fcdb519b2466f47884409320a9f
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2023-08-06 14:21:25 +0900

    add style

--- aws_lb_listener_rule.tf
+++ aws_lb_listener_rule.tf
@@ -28,7 +28,7 @@ resource "aws_lb_listener_rule" "main_subdomain" {
   }
 }
 
-resource "aws_lb_listener_rule" "main_fixed" {
+resource "aws_lb_listener_rule" "main_fixed_style" {
   for_each = toset([
     aws_lb_listener.main_https.arn,
     aws_lb_listener.main_http.arn,
@@ -43,13 +43,50 @@ resource "aws_lb_listener_rule" "main_fixed" {
     }
   }
 
+  condition {
+    path_pattern {
+      values = ["/fixed.css"]
+    }
+  }
+
   action {
     type = "fixed-response"
 
     fixed_response {
-      content_type = "text/plain"
+      content_type = "text/css"
+      status_code  = "200"
+      message_body = templatefile("${path.module}/templates/fixed.css", {})
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "main_fixed" {
+  for_each = toset([
+    aws_lb_listener.main_https.arn,
+    aws_lb_listener.main_http.arn,
+  ])
+
+  listener_arn = each.value
+  priority     = 25
+
+  condition {
+    host_header {
+      values = [local.redirector_fixed_domain_name]
+    }
+  }
+
+  action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
       status_code  = "410"
-      message_body = "${local.redirector_fixed_domain_name} has been permanently removed."
+      message_body = templatefile(
+        "${path.module}/templates/fixed.html",
+        {
+          domain_name = local.redirector_fixed_domain_name
+        }
+      )
     }
   }
 }
